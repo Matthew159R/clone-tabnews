7c0016f9854d20e7c714ac80f422e3d9e1d11a45
Matheus Batista <117731536+Matthew159R@users.noreply.github.com>
2023-09-16 20:56:47 +0000

    Muda a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mãe, eu amo você! Se você me ama, da uma risadinha! 😘</h1>;
+    return <h1>Irmã, eu te amo! Se você me ama da uma risadinha! 😘 😘 😘 😘 </h1>;
 }
 
 export default Home;
